d6f9ef058fdc606930344085a15509b9b8743094
Carlos Guzman <cg546289@gmail.com>
2023-09-07 18:28:53 +0000

    linking ui

--- sapui5/webapp/controller/App.controller.js
+++ sapui5/webapp/controller/App.controller.js
@@ -7,6 +7,33 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
+        function onInitv2(){
+            var oJSONModel = new sap.ui.model.json.JSONModel(); //set JSON
+            var oView = this.getView(); //get JSON
+            var i18nBundle = oView.getModel("i18n").getResourceBundle();
+            var oJSON = { //Model json
+                employeeId : "12345",
+                countryKey : "UK",
+                listCountry: [
+                    {
+                        key : "U",
+                        text : i18nBundle.getText("countryUS") 
+                    },
+                    {
+                        key : "UK",
+                        text : i18nBundle.getText("countryUK") 
+                    },
+                    {
+                        key : "ES",
+                        text : i18nBundle.getText("countryES") 
+                    }
+                ]
+            };
+
+            oJSONModel.setData(oJSON); //pass the data of type JSON
+            oView.setModel(oJSONModel);
+        }
+
         function myCheck(){
             var inputEmployee = this.byId("inputEmployee");
             var valueEmployee = inputEmployee.getValue();
@@ -21,10 +48,7 @@ sap.ui.define([
         }
 
         return Controller.extend("aa.sapui5.controller.App", {
-            onInit: function () {
-
-            },
-
-            onValidate : myCheck
+            onValidate : myCheck,
+            onInit : onInitv2
         });
     });
