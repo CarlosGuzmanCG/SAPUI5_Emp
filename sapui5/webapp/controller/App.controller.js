65728671065c67dc186ccd1ad30a5be7a3d89a6d
Carlos Guzman <cg546289@gmail.com>
2023-09-07 17:19:16 +0000

    elements and controllers

--- sapui5/webapp/controller/App.controller.js
+++ sapui5/webapp/controller/App.controller.js
@@ -7,9 +7,24 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
+        function myCheck(){
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if(valueEmployee.length === 10 ){
+                this.byId("labelCountry").setVisible(true);
+                this.byId("slCountry").setVisible(true);
+            }else{
+                this.byId("labelCountry").setVisible(false);
+                this.byId("slCountry").setVisible(false);
+            }
+        }
+
         return Controller.extend("aa.sapui5.controller.App", {
             onInit: function () {
 
-            }
+            },
+
+            onValidate : myCheck
         });
     });
