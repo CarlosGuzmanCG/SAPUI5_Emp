ec983ed3637792b38449f35007764a70834f77f1
Carlos Guzman <cg546289@gmail.com>
2023-09-11 17:54:41 +0000

    lists

---
+++ lists/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["aa/lists/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ lists/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"aa/lists/test/unit/controller/ListTypes.controller"
+], function () {
+	"use strict";
+});
