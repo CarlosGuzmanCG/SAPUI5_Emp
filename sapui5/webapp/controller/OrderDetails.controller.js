61977a7e3d110a6119d4f30cb1f9ac2f373b1b49
Carlos Guzman <cg546289@gmail.com>
2023-09-22 23:27:53 +0000

    browsing history

--- sapui5/webapp/controller/OrderDetails.controller.js
+++ sapui5/webapp/controller/OrderDetails.controller.js
@@ -1,12 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function (Controller) {
+    "sap/ui/core/routing/History"
 
+], function (Controller, History) {
+
+    function onBack(){
+        var oHistory = History.getInstance();
+        var sPreviosHash = oHistory.getPreviousHash();
+        if(sPreviosHash !== undefined){
+            window.history.go(-1);
+        }else{
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("RouteMain", true);
+        };
+    }
 
     return Controller.extend("aa.sapui5.controller.OrderDetails", {
         onInit: function () {
             
-        }
+        },
+
+        onBack : onBack
 
     });
 
