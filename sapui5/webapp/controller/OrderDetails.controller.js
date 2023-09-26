503059e1ef42a754ceafcbf357ee2023080e53b6
Carlos Guzman <cg546289@gmail.com>
2023-09-26 20:59:41 +0000

    factory

--- sapui5/webapp/controller/OrderDetails.controller.js
+++ sapui5/webapp/controller/OrderDetails.controller.js
@@ -27,6 +27,34 @@ sap.ui.define([
         signature.clear();
     }
 
+    function factoryOrderDetails (listId, oContext){
+        var contextObject = oContext.getObject();
+        contextObject.Currency = "EUR";
+        var unitsInStock = oContext.getModel().getProperty("/Products(" + contextObject.ProductID + ")/UnitsInStock");
+
+        if (contextObject.Quantity <= unitsInStock) {
+            var objectListItem = new sap.m.ObjectListItem({
+                title : "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})",
+                number : "{parts: [ {path: 'odataNorthwind>UnitPrice'}, {path: 'odataNorthwind>Currency'}], type:'sap.ui.model.type.Currency', formatOptions: {showMeasure: false}}",
+                numberUnit: "{odataNorthwind>Currency}"
+            });
+            return objectListItem;
+        } else {
+            var customListItem = new sap.m.CustomListItem({
+                content: [
+                    new sap.m.Bar({
+                        contentLeft: new sap.m.Label({ text: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})"}),
+                        contentMiddle:new sap.m.ObjectStatus({ text: "{i18n>availableStock} {odataNorthwind>/Products(" + contextObject.ProductID + ")/UnitsInStock}", state: "Error"}),
+                        contentRight: new sap.m.Label({text:"{parts: [ {path: 'odataNorthwind>UnitPrice'}, {path: 'odataNorthwind>Currency'}], type:'sap.ui.model.type.Currency'}"}) 
+                    })
+                ]
+            });
+            return customListItem;
+        }
+    }
+
+
+
     return Controller.extend("aa.sapui5.controller.OrderDetails", {
         onInit: function () {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this); //routing
@@ -34,8 +62,8 @@ sap.ui.define([
         },
 
         onBack : onBack,
-        onClearSignature:onClearSignature
-
+        onClearSignature:onClearSignature,
+        factoryOrderDetails:factoryOrderDetails
     });
 
 });
